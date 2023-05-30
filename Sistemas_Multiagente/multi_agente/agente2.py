9422f7ae638b44d45717f9b3e9c94b4387344b95
Gugonunes <gugonunes@hotmail.com>
2023-05-30 17:59:54 -0300

    add comportamento vendedor

---
+++ Sistemas_Multiagente/multi_agente/agente2.py
@@ -0,0 +1,21 @@
+from pade.misc.utility import display_message, start_loop
+from pade.core.agent import Agent
+from pade.acl.aid import AID
+from pade.behaviours.protocols import TimedBehaviour
+from pade.acl.messages import ACLMessage
+from pade.behaviours.protocols import FipaRequestProtocol
+from sys import argv
+
+class ComportamentoVendedor(FipaRequestProtocol):
+    def __init__(self, agent):
+        super(ComportamentoVendedor, self).__init__(agent=agent, message=None, is_initiator=False)
+
+        self.produtos = 'TV 55', 'Notebook', 'Microondas'
+
+    def handle_request(self, message):
+        super(ComportamentoVendedor, self).handle_request(message)
+        display_message(self.agent.aid.localname, 'Recebi a requisição')
+        resposta = message.create_reply()
+        resposta.set_performative(ACLMessage.INFORM)
+        resposta.set_content(self.produtos)
+        self.agent.send(resposta)
